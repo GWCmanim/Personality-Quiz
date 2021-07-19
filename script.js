ec57f19fc0760ed12328713cd48d2989dd240611
ImaniMitchell <87670009+GWCmanim@users.noreply.github.com>
2021-07-19 20:17:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Add your JavaScript here*/
+ 
+
+
+var Nebula = 0; // Store the Nebula 
+var Drax = 0; // Store the Drax
+var Loki = 0; // Store the Loki
+var Spiderman = O; // Store the Spiderman
+var Groot = 0; // Store the Groot
+var Wanda = 0 ; // Store the Wanda 
+var StarLord = 0; // Store the StarLord
+
+document.getElementById("result").innerHTML = "Loki";
+
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById ("q1a2")
+var q1a3 = document.getElementById ("q1a3")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
